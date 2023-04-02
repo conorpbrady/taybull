a49b15cbe2d3f6aa7d5c716e659200e86cf16207
Conor Brady <conorpbrady@gmail.com>
2023-04-02 10:55:40 -0400

    add pseudocode

---
+++ main.py
@@ -0,0 +1,38 @@
+import selenium
+
+class Site:
+    pass
+
+class Tock(Site):
+    pass
+
+class Redy(Site):
+    pass
+
+class OpenTable(Site):
+    pass
+
+
+
+def main():
+    """
+    Flat file  with sites, URLs, and platform
+    Read file with preferred dates/times
+    env file with logins / pws / cc data?
+
+    Launch Sign in Page
+    Login
+    Launch Reservations Page
+    Select with priority of preferred times
+    Select Res
+    Go through appropriate flow based off platform
+
+
+
+
+    Phase 2:
+    Read activation times - know when to fire
+
+
+if __name__ == '__main__':
+    main()
