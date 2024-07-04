d35d05828ef7b1dabb0299c133ce8ff45afb0427
Conor Brady <conorpbrady@gmail.com>
2024-07-04 14:38:01 -0400

    fix platform import

--- resrvar/bookingengine/tock.py
+++ resrvar/bookingengine/tock.py
@@ -1,7 +1,7 @@
 from bookingengine.resplatform import ResPlatform
 from datetime import datetime, timedelta
 import logging
-from system import platform
+from sys import platform
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
